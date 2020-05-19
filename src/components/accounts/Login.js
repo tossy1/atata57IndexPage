499c79bda3a5b32d28c132acfeaafcb5ffd031d8
Are Oluwasegun <37227404+AceRex@users.noreply.github.com>
2020-05-19 15:44:44 +0100

    Update Login.js

--- src/components/accounts/Login.js
+++ src/components/accounts/Login.js
@@ -1,13 +1,23 @@
 import React from "react";
 import "../accounts/account.css";
+import styled from 'styled-components'
 import { Link } from "react-router-dom";
 
 function Login() {
   return (
-    <div>
-      <span> Login </span>
-    </div>
+    <LoginDiv>
+      <div className
+        
+        
+
+      </LoginDiv>
   );
 }
+
+const LoginDiv = styled.div`
+width: 70%;
+margin: auto;
+
+`
 
 export default Login;
