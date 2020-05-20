097ccdecc87e2a50c0ba8b68a70ffa94eb403a39
Are Oluwasegun <37227404+AceRex@users.noreply.github.com>
2020-05-20 14:03:54 +0100

    staright up

--- src/components/accounts/registerPages/registerUser.js
+++ src/components/accounts/registerPages/registerUser.js
@@ -1,5 +1,9 @@
 import React, { Component } from "react";
-import { CountryDropdown, RegionDropdown, CountryRegionData } from 'react-country-region-selector';
+import {
+  CountryDropdown,
+  RegionDropdown,
+  CountryRegionData,
+} from "react-country-region-selector";
 
 import Header from "../../header/header";
 import Footer from "../../footer/footer";
@@ -8,7 +12,7 @@ import styled from "styled-components";
 export default class UserReg extends Component {
   constructor(props) {
     super(props);
-    this.state = { country: '', region: '' };
+    this.state = { country: "", region: "" };
   }
 
   selectCountry(val) {
@@ -23,37 +27,47 @@ export default class UserReg extends Component {
     return (
       <>
         <Register>
-          <h3>Registration Form</h3>
           <div className="UserProfile">
-            <p>User Profile</p>
+          <h3>Registration Form</h3>
             <hr />
-            <div className="UserForm row">
-              <div className="d-flex">
+            <div className=" row">
+              <div className="form-group d-flex">
                 <input type="text" placeholder="First Name" />
                 <input type="text" placeholder="Middle Name" />
                 <input type="text" placeholder="Last Name" />
               </div>
-              <div className="d-flex">
+              <div className="form-group d-flex">
                 <input type="email" placeholder="Email" />
                 <input type="email" placeholder="Retype-email" />
                 <input type="number" placeholder="Phone number" />
               </div>
-              <div className="d-flex">
+              <div className="form-group d-flex">
+                <i className=" fas fa-globe"></i>
                 <CountryDropdown
                   value={country}
-                  onChange={(val) => this.selectCountry(val)} />
+                  onChange={(val) => this.selectCountry(val)}
+                />
                 <RegionDropdown
                   country={country}
                   value={region}
-                  onChange={(val) => this.selectRegion(val)} />
+                  onChange={(val) => this.selectRegion(val)}
+                />
+              </div>
+              <div className="form-group address d-flex">
+                <i className=" fas fa-locator"></i>
+                <input type="text" placeholder="Address" />
+                <input type="text" placeholder="Delivery Address" />
               </div>
             </div>
+            <div className="form-group">
+              <button>Proceed</button>
+            </div>
           </div>
-          <div className="Residence"></div>
+         
         </Register>
 
-        {/* <Header />
-        <Footer /> */}
+        <Header />
+        <Footer />
       </>
     );
   }
@@ -62,8 +76,55 @@ export default class UserReg extends Component {
 const Register = styled.div`
   width: 70%;
   margin: auto;
+  .UserProfile{
+    margin-top:10rem;
+    margin-bottom: 5rem;
+  }
   h3 {
     color: var(--colorAsh);
     font-size: 30px;
+  }
+  .form-group {
+    width: 90%;
+    margin: 20px auto;
+    i{
+      position: absolute;
+      top: 375px;
+      left: 250px;
+      font-size: 17px;
+      color:var(--colorAsh);
+    }
+    input {
+      border: solid 0.5px var(--colorAsh);
+      background-color: var(--colorLight);
+      padding: 10px;
+      width: 50%;
+      margin: 0 5px;
+      :focus{
+        outline-color: var(--colorYellow);
+      }
+    }
+    select {
+      border: solid 0.5px var(--colorAsh);
+      background-color: var(--colorLight);
+      padding: 10px;
+      padding-left: 50px;
+      width: 50%;
+      margin: 0 5px;
+      :focus{
+        outline-color: var(--colorYellow);
+      }
+    }
+    button{
+      margin-left: 80%;
+      padding: 10px;
+      width: 20%;
+      border: none;
+      background-color: var(--colorGreen);
+      color: #fff;
+      :hover{
+        background-color: var(--colorBlack);
+      }
+    }
   }
 `;
