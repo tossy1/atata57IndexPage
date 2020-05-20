e73987853a2679174297aab348a4e7ab9e506958
Are Oluwasegun <37227404+AceRex@users.noreply.github.com>
2020-05-20 14:26:18 +0100

    straight up

--- src/components/accounts/registerPages/RegisterMain.js
+++ src/components/accounts/registerPages/RegisterMain.js
@@ -1,10 +1,9 @@
 import React, { Component } from "react";
 import "../account.css";
-import Container from "react-bootstrap/Container";
 import { Link } from "react-router-dom";
 import Header from "../../header/header";
-import Footer from "../../footer/footer"
-import Row from "react-bootstrap/Row";
+import Footer from "../../footer/footer";
+import styled from "styled-components";
 
 class Register extends Component {
   render() {
@@ -12,29 +11,24 @@ class Register extends Component {
       <>
         <div className="registerMain">
           <p class="regH">Choose an Account Type</p>
-          <Container>
-            <Row>
-              <Link to="/IndividualRegistration" className="accountCard user">
-                <i class="fas fa-user"></i>
-                <p>Buyer</p>
-              </Link>
-              <Link to="/CompanyReg" class="accountCard company">
-                <i class="fas fa-building"></i>
-                <p>Service Provider</p>
-              </Link>
-            </Row>
-            <Row>
-              <Link to="/ProductSellerRegistration" class="accountCard seller">
-                <i class="fas fa-briefcase"></i>
-                <p>Products Suppliers</p>
-              </Link>
-              <Link to="/WarehouseRegistration" class="accountCard warehouse">
-                <i class="fas fa-warehouse"></i>
-                <p>Warehouse</p>
-              </Link>
-            </Row>
-         
-          </Container>
+          <Contains>
+            <Link to="/IndividualRegistration" className="accountCard user">
+              <i class="fas fa-user"></i>
+              <p>Buyer</p>
+            </Link>
+            <Link to="/CompanyReg" class="accountCard company">
+              <i class="fas fa-building"></i>
+              <p>Service Provider</p>
+            </Link>
+            <Link to="/ProductSellerRegistration" class="accountCard seller">
+              <i class="fas fa-briefcase"></i>
+              <p>Products Suppliers</p>
+            </Link>
+            <Link to="/WarehouseRegistration" class="accountCard warehouse">
+              <i class="fas fa-warehouse"></i>
+              <p>Warehouse</p>
+            </Link>
+          </Contains>
           <div
             className="register"
             style={{ margin: "5% auto", padding: "2%" }}
@@ -48,10 +42,27 @@ class Register extends Component {
           </div>
         </div>
         <Header />
-        {/* <Footer /> */}
+        <Footer />
       </>
     );
   }
 }
+
+const Contains = styled.div`
+  display: flex;
+  margin-top: 20px;
+  i{
+    font-size: 40px;
+  }
+  a{
+    text-align: center;
+    padding: 30px;
+    margin: 20px;
+    width: 20%;
+    p{
+      font-size: 17px;
+    }
+  }
+`;
 
 export default Register;
