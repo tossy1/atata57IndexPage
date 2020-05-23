b611126eb409fc7279d028cd295dc866b7ea9d75
Are Oluwasegun <37227404+AceRex@users.noreply.github.com>
2020-05-23 13:24:39 +0100

    Update TopSelling.js

--- src/components/Pages/TopSelling.js
+++ src/components/Pages/TopSelling.js
@@ -9,14 +9,31 @@ import styled from "styled-components";
 class TopSelling extends Component {
   render() {
     return (
-        <div>
-            <Header />
-            <Footer />
+      <TopSellingContainer>
+        <div className="banner">
+
         </div>
+        <div className="col-12">
+          <div className="row">
+            <div className="col-3">Hello</div>
+            <div className="col-9">Hello</div>
+          </div>
+        </div>
+        <Header />
+        <Footer />
+      </TopSellingContainer>
     );
   }
 }
 
-
+const TopSellingContainer = styled.div`
+margin-top: 100px;
+.banner{
+  background-color: var(--colorAsh);
+  width: 90%;
+  margin: auto;
+  height: 100px;
+}
+`
 
 export default TopSelling;
